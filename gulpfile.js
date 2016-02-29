1fa147ae662d90a7d8adf3d581d4a259c594532a
spacemacgyver <macgyverswissarmyknife@gmail.com>
2016-02-29 12:39:15 +0000

    Got mocha working

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp'),
     mocha = require('gulp-mocha');
 
-gulp.task('mocha', () => {
-    return gulp.src('*/test/**/*.spec.js', {read: false})
-        .pipe(mocha({}));
+require('babel-core/register');
+
+gulp.task('mocha', [], function () {
+    return gulp.src('./*/test/**/*.spec.js')
+        .pipe(mocha({
+        }));
 });
---
+++ slidereal-core/test/frame.spec.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('Feature: Frame model', () => {
+    describe('Scenario: Model layout', () => {
+        let currentFrame;
+        beforeEach('Given a frame', () => {});
+    });
+});
